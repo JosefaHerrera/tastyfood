c2b842f23d48a57bff141662872cb705b760cd24
Anayn Pavez <nanyn.pavez@gmail.com>
2017-08-16 22:54:02 -0300

    modificación de sección discover

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,7 +20253,84 @@ if (jQuery) {
   };
 })(jQuery);
 
+var apiKey = "4e7a74042a2f392bc1d0435f749f26b6";
+	$.ajax({
+		url: 'https://developers.zomato.com/api/v2.1/location_details',
+		type: 'GET',
+		dataType: 'json',
+		beforeSend: function(request){
+	        request.setRequestHeader("Content-Type","application/json");
+	        request.setRequestHeader("user-key", apiKey);
+	    },
+	    data : {
+	    	'entity_id' : '83',
+	    	'entity_type' : 'city'
+	    }
+	})
+	.done(function(res) {
+		res.best_rated_restaurant.forEach(function(ele){
+			console.log(ele);
+			var img = ele.restaurant.thumb;
+			var name = ele.restaurant.name;
+			var comuna = ele.restaurant.location.locality;
+			console.log(img);
+			$(".contenedorjson").append("<li><div class='card'><div class='card-image'><img src='"+img+"'></div><div class='card-content'><i class='fa fa-cutlery' aria-hidden='true'></i><p class='nombreR'>"+name+"</p><br><i class='fa fa-map-marker' aria-hidden='true'></i><p class='comuna'>"+comuna+"</p></div></div></li>");
+		})
+	})
+	.fail(function(response) {
+		console.log(response);
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
+
   // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+  // This example requires the Places library. Include the libraries=places
+  // parameter when you first load the API. For example:
+  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+  var map;
+  var infowindow;
+
+  function initMap() {
+    var pyrmont = {lat: -33.4569400, lng: -70.6482700};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['restaurant']
+    }, callback);
+  }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
