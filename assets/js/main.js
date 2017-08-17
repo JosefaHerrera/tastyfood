e63e9273137258e479621a342622afaa5c97a732
Antonia Cerda <antoniacerdag@gmail.com>
2017-08-17 10:50:36 -0300

    maqueta con info del restaurant

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,14 +28,28 @@ $(document).ready(function() {
 	        }
 	    })
 	    .done(function(res) {
+	    	console.log(res);
 	        res.best_rated_restaurant.forEach(function(ele){
 	            console.log(ele);
 	            var img = ele.restaurant.thumb;
 	            var name = ele.restaurant.name;
 	            var comuna = ele.restaurant.location.locality;
-	            $(".contenedorjson").append("<li><div class='card'><div class='card-image'><a class='imgdetails' type='button'><img src='"+img+"'></a></div><div class='card-content'><i class='fa fa-cutlery' aria-hidden='true'></i><p class='nombreR'>"+name+"</p><i class='fa fa-map-marker' aria-hidden='true'></i><p class='comuna'>"+comuna+"</p></div></div></li>");
+	            var precio = ele.restaurant.average_cost_for_two;
+	            var rating = ele.restaurant.user_rating.aggregate_rating;
+	            $(".contenedorjson").append("<li><div class='card'><div class='card-image'><a class='imgdetails' type='button'><img src='"+img+"'></a></div><div class='card-content'><a type='button' class='restdetails'><i class='fa fa-cutlery' aria-hidden='true'></i></a><p class='nombreR'>"+name+"</p><i class='fa fa-map-marker' aria-hidden='true'></i><p class='comuna'>"+comuna+"</p></div></div></li>");
 	            
-	            $(".imgdetails").click(function(e){
+	            $(".imgdetails").click(function(){
+	            	$(".info").empty();
+	            	$(".info").append("<div class='franjanaranja'><div class='row'><div class='col s8'><p class='nomRest'>"+name+"</p></div><div class='col s4 right-align'><i class='fa fa-heart' aria-hidden='true'></i></div></div><div class='cajablanca'><div class='row'><div class='col s12'><p class='address'>Address</p></div></div><div class='row'><div class='col s12'><p class='ubicacionrest'>"+comuna+"</p></div></div><div class='row'><div class='col s12'><p class='precio'>Precio</p></div></div><div class='row'><div class='col s12'><p class='preciorest'>"+precio+"</p></div></div><div class='row'><div class='col s12'><p class='rating'>Rating</p></div></div><div class='row'><div class='col s12'><p class='ratingrest'>'"+rating+"</p></div></div></div>");
+
+	            	$(".fa-heart").click(function(){
+	            		$(this).css('color', 'tomato');
+	            	})
+	            })
+
+	            
+
+	             $(".restdetails").click(function(){
 	            	$(".info").append(name);
 	            })
 
