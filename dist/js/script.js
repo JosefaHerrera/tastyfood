13b271e663690eafe894386153304bf061c1ee04
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-08-18 09:13:15 -0300

    detalles

--- dist/js/script.js
+++ dist/js/script.js
@@ -20258,24 +20258,29 @@ if (jQuery) {
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
 
+/*INICIO validaciones*/
   $(document).ready(function(){
   	$("#iniciar-sesion").click(function(){
   		var valEmail = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
   		var valPass = /^\d{6}([0-9])*$/;
-
+      //validar campos vacios
   		if($('.form-control').val().length == 0 || $('.form-control').val().length === ""){
   			$(".name-error").append('<span>Invalid name</span>');
   			return false;
   		}
+      //validar email
   		if(!valEmail.test($('#email-signup').val().trim())){
         $(".email-error").append('<span>Invalid email</span>');
   		}
+      //validar contraseña
   		if(!valPass.test($('#pass-signup').val().trim())){
   			$(".pass-error").append('<span>Invalid Password (min 6 characters)</span>');
   		}
+      //enviar a la siguiente página
   		else{
   			window.location.href = 'search.html';
   		}
 
   	});		
   });
+/*FIN validaciones*/
